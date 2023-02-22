7fb2256c84d5d52afa4aab5831c998b0fa994580
Anderson Pozo <anderson.pozo@upec.edu.ec>
2023-02-22 09:58:56 -0500

    implement services to find users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Logger, InternalServerErrorException, BadRequestException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserInput } from './dto';
@@ -6,6 +7,8 @@ import { User } from './entities';
 
 @Injectable()
 export class UserService {
+
+    private logger = new Logger('UserService');
 
     constructor(
         @InjectRepository(User)
@@ -16,10 +19,49 @@ export class UserService {
         return this.userRepository.find();
     }
 
+    async findOne({ id, username }: { id?: string, username: string }){
+        try {
+            return await this.userRepository.findOneByOrFail({ id, username });
+        } catch (error) {
+            this.handleErrors({
+                code: 'error-404',
+                detail: `Usuario ${ username } no encontrado`
+            })
+        }
+    }
+    
+    async findOneById(id: string){
+        try {
+            return await this.userRepository.findOneByOrFail({ id });
+        } catch (error) {
+            this.handleErrors({
+                code: 'error-404',
+                detail: `Usuario con id ${ id } no encontrado`
+            })
+        }
+    }
+
     async create(createUserInput: CreateUserInput){
-        const user = this.userRepository.create(createUserInput);
+        const user = this.userRepository.create({
+            ...createUserInput,
+            password: bcrypt.hashSync(createUserInput.password, 10)
+        });
 
         return await this.userRepository.save(user);
+    }
+
+    private handleErrors(error: any): never{
+        
+        this.logger.error(error)
+
+        if (error.code === '23505'){
+            throw new BadRequestException(error.detail);
+        }
+        if (error.code === 'error-404') {
+            throw new NotFoundException(error.detail)
+        }
+        
+        throw new InternalServerErrorException('Check server logs')
     }
     
 }
